f52765edfb712d59906636231241852a419d2477
srikanta.gouda <srikanta.gouda@orientcap.com>
2025-04-25 09:09:17 +0530

    updated start script

--- start_scripts.py
+++ start_scripts.py
@@ -2,7 +2,7 @@ import subprocess
 
 def run_script(script_name):
     print(f"Starting {script_name}...")
-    subprocess.Popen(["python", script_name], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    subprocess.Popen(["python", script_name])
 
 if __name__ == "__main__":
     print("123 LoadBoard Automation")
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     run_script("data_fetcher.py")
     run_script("email_processor.py")
     
-    print("All scripts launched. Press any key to exit this window...")
+    print("All scripts launched. Press Enter to exit...")
     input("Press Enter to exit...")
